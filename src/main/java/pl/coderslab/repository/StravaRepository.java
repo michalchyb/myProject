65d1cfc2096377050ac7cd5b33515e839a9a98e1
Michał Chyb <michalchyb1@gmail.com>
2018-06-17 18:11:31 +0200

    delete unused files

--- src/main/java/pl/coderslab/repository/StravaRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.coderslab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.coderslab.entity.City;
-
-public interface StravaRepository extends JpaRepository<City, Long> {
-
-}
