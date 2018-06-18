100ead5af34300405c1df6ac6425412032890ff6
Michał Chyb <michalchyb1@gmail.com>
2018-06-18 17:08:34 +0200

    error page added

--- src/main/java/pl/coderslab/MyProjectApplication.java
+++ src/main/java/pl/coderslab/MyProjectApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import pl.coderslab.entity.City;
 import pl.coderslab.entity.RunImport;
 
@@ -16,7 +18,11 @@ import pl.coderslab.entity.RunImport;
 import java.io.IOException;
 
 @SpringBootApplication
-public class MyProjectApplication {
+public class MyProjectApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyProjectApplication.class, args);
--- src/main/java/pl/coderslab/web/HomeController.java
+++ src/main/java/pl/coderslab/web/HomeController.java
@@ -2,6 +2,7 @@ package pl.coderslab.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -28,18 +29,18 @@ public class HomeController {
 //        return "about";
 //    }
 
-    @GetMapping("/404")
-    public String error404() {
-        return "404";
-    }
-    @GetMapping("/405")
-    public String error405() {
-        return "405";
-    }
-    @GetMapping("/500")
-    public String error500() {
-        return "500";
-    }
+//    @GetMapping("/404")
+//    public String error404() {
+//        return "errors/404";
+//    }
+//    @GetMapping("/405")
+//    public String error405() {
+//        return "errors/405";
+//    }
+//    @GetMapping("/500")
+//    public String error500() {
+//        return "errors/500";
+//    }
     @GetMapping("/contact")
     public String contact() {
         return "contact";
@@ -47,5 +48,13 @@ public class HomeController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+    @Controller
+    public class MyController {
+
+        @RequestMapping("/")
+        public void handleRequest() {
+            throw new RuntimeException("test exception");
+        }
     }
 }
